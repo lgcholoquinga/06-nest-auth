13f073e49910293fb4af98c0778ccc7add3bcf7f
lgcholoquinga <luisg.choloquingal@gamil.com>
2023-07-13 17:05:43 -0500

    Implementation login with JWT

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,86 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+import * as bcryptjs from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { LoginResponse } from './interfaces/login-response.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData,
+      });
+
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists.`);
+      }
+      throw new InternalServerErrorException('Something wrong!!!');
+    }
+  }
+
+  async login(loginDto: LoginDto): Promise<LoginResponse> {
+    const { email, password } = loginDto;
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials.');
+    }
+
+    if (!bcryptjs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Invalid credentials.');
+    }
+
+    const { password: _, ...userData } = user.toJSON();
+
+    return {
+      user: userData,
+      token: this.getJWT({ id: user.id }),
+    };
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateAuthDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+
+  getJWT(payload: JwtPayload): string {
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
+}
